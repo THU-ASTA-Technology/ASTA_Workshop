cdad032e2dafe07fa9efaf56dfecbbe3c9f7ebf6
HuTao今天吃什么 <1184726415@qq.com>
2023-02-03 18:49:01 +0800

    modified:   moz-todo-react/src/App.css 	modified:   moz-todo-react/src/App.js 	modified:   moz-todo-react/src/bar/Bar.js 	modified:   moz-todo-react/src/utils.css

--- moz-todo-react/src/bar/Bar.js
+++ moz-todo-react/src/bar/Bar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Bar.css";
 const Bar = (props) => {
-    return (<div className="bar-container">
-        <div className="bar-title">
-            
-        </div>
-    </div>);
+    return (
+        <div className="bar-container">
+            <div className="bar-title">
+                导航栏 is here
+            </div>
+        </div>);
 };
 export default Bar;
