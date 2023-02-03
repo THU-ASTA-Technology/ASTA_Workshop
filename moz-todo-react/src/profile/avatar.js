9f4f214b7f1b52eb1414eca8656e619f80194fb1
HuTao今天吃什么 <1184726415@qq.com>
2023-02-03 18:18:36 +0800

    modified:   moz-todo-react/src/profile/avatar.js 	modified:   moz-todo-react/src/profile/profile.js

---
+++ moz-todo-react/src/profile/avatar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./avatar.css";
+
+const avatar=(props)=>{
+    return (
+        <div className="avatar"></div>
+    );
+};
+export default avatar;
---
+++ moz-todo-react/src/profile/profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./profile.css";
+const profile=(props)=>{
+    return (
+        <div className="profile"></div>
+    );
+};
+export default profile;
