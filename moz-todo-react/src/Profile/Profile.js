595798e7393e425a6c4c4b7af2810dbb99e90b85
HuTao今天吃什么 <1184726415@qq.com>
2023-02-04 17:18:43 +0800

    modified:   moz-todo-react/README.md deleted:    moz-todo-react/package-lock.json 	new file:   moz-todo-react/src/Bar/Bar.css 	new file:   moz-todo-react/src/Bar/Bar.js 	new file:   moz-todo-react/src/Profile/Avatar.js 	new file:   moz-todo-react/src/Profile/Profile.css 	new file:   moz-todo-react/src/Profile/Profile.js 	deleted:    moz-todo-react/src/profile/Avatar.css 	new file:   moz-todo-react/yarn.lock

---
+++ moz-todo-react/src/Profile/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Profile.css";
+import Avatar from "./Avatar.js";
+const Profile = (props) => {
+    return (
+        <div className="profile">
+            <div className="profile-avatar">
+                <Avatar />
+            </div>
+            <div className="profile-name u-textCenter">
+
+            </div>
+            <div className="profile-item u-flexColumn">
+
+            </div>
+        </div>
+    );
+};
+export default Profile;
