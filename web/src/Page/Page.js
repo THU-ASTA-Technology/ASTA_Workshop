0986757f3044ba9853acd9556b4aa70dde226eff
xuyy21 <xuyy21@mails.tsinghua.edu.cn>
2023-02-05 19:06:14 +0800

    Home & Page

---
+++ web/src/Page/Page.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Page.css";
+
+const Page = (props) => {
+    return (
+        <div className="Page-container">
+            {props.children}
+        </div>
+    );
+};
+
+
+const NotFound = (props) => {
+    return (
+        <h1>Not Found</h1>
+    );
+};
+
+export { Page, NotFound };
