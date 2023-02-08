12a2b5343f7aa6b38d7a5ba318b7f833498a0a44
whz21 <whz21@mails.tsinghua.edu.cn>
2023-02-08 13:31:09 +0800

    step4

---
+++ web/src/Tape/TapeEdit.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { post } from "../utils";
+import "./TapeEdit.css";
+
+const defaultTape = {
+    query: ""
+};
+
+const TapeEdit = (props) => {
+    const defaultQuery = "Tape here";
+    const [tape, setTape] = useState(defaultTape);
+    
+    const handleQueryChange = (event) => {
+        setTape({...tape, query: event.target.value});
+    };
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        post("tape/edit/", tape)
+            .then(() => navigate("/tape"))
+            .catch((error) => console.log(error));
+    };
+
+    return (
+        <div className="TapeEdit-container u-flexColumn">
+            <input placeholder={defaultQuery} value={tape.query} onChange={handleQueryChange}></input>
+            <button className="button" onClick={handleSubmit}>Submit</button>
+        </div>
+    );
+};
+
+export default TapeEdit;
