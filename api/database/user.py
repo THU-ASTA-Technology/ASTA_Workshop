ed7d45b5f8326bf56a74cfdd039a00ef8129e544
赵若帆 <3186168293@qq.com>
2023-02-09 16:04:44 +0800

    Update user.py

--- api/database/user.py
+++ api/database/user.py
@@ -5,3 +5,15 @@ from django.contrib import auth
 
 # To do for the later lecture.
 # Also you can take it as a practice.
+# TODO: complete the functions below
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def register(request):
+    pass
+
+def getInfo(request):
+    pass
