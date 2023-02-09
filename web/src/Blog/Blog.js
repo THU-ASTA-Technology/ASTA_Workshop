6070bdb76ac5eb3b145b4e11e6833bc8df69db19
赵若帆 <3186168293@qq.com>
2023-02-09 16:30:25 +0800

    Update Blog.js

--- web/src/Blog/Blog.js
+++ web/src/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Blog.css";
 const Blog = (props) => {
+    //TODO: add blog logic, also remember to update Tape and Broadcast so that they can adapt to User
     return (
         <div>
             <h1>Your code here</h1>
